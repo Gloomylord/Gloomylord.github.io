1057d3e496221dcc8773e6d1d24719e4b064bb2d
Dimitry <43682392+Gloomylord@users.noreply.github.com>
2020-05-20 12:23:16 +0300

    changed smile color

---
+++ page/precache-manifest.0e2706919a38e1deeb67c52096b321d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca13a8d31a1285b761b6b94e0c1db716",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "29197868fa5461c07cba",
+    "url": "/page/static/css/main.04ace252.chunk.css"
+  },
+  {
+    "revision": "00a42045dcf3e7489991",
+    "url": "/page/static/js/2.e47a99af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/page/static/js/2.e47a99af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29197868fa5461c07cba",
+    "url": "/page/static/js/main.88e0ad93.chunk.js"
+  },
+  {
+    "revision": "4a535f3822b97a643d61",
+    "url": "/page/static/js/runtime-main.6a14b105.js"
+  }
+]);
