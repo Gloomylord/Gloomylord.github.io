c2ad802358d67836a570097561e0e4c14129c73f
Dimitry <43682392+Gloomylord@users.noreply.github.com>
2020-05-28 20:34:19 +0300

    Add new page

---
+++ App/precache-manifest.2a46165e2593bb034f3555caea55b574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d131eba9c807f9569c9fa901efc08f38",
+    "url": "/App/index.html"
+  },
+  {
+    "revision": "b23344330ebec2851e6d",
+    "url": "/App/static/css/main.2664aa01.chunk.css"
+  },
+  {
+    "revision": "e1e35287cbac193b020a",
+    "url": "/App/static/js/2.fc5a9837.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/App/static/js/2.fc5a9837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23344330ebec2851e6d",
+    "url": "/App/static/js/main.c30f434f.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/App/static/js/runtime-main.c8f0954f.js"
+  }
+]);
