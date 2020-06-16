adf8d5ef6198ea055b367b566aa45da65c3a4e44
Dimitry <43682392+Gloomylord@users.noreply.github.com>
2020-06-16 13:57:50 +0300

    css and vanilla js

---
+++ css/precache-manifest.a031b667e6ae8e04db5e811ad487b244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f8b3537adcb287d45e34fe43cf2357",
+    "url": "/css/index.html"
+  },
+  {
+    "revision": "7793f90ac6033a64841d",
+    "url": "/css/static/css/main.7cb32cba.chunk.css"
+  },
+  {
+    "revision": "7227d77fd6063e3f7bc9",
+    "url": "/css/static/js/2.7a789d7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/css/static/js/2.7a789d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7793f90ac6033a64841d",
+    "url": "/css/static/js/main.9c2a6e04.chunk.js"
+  },
+  {
+    "revision": "b2439b03f322d496e0d2",
+    "url": "/css/static/js/runtime-main.a95be436.js"
+  }
+]);
