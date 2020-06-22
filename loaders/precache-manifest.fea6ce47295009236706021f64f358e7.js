cb2327ebb9ce8d7b87bbdb6cea6341776b8d37ad
Dimitry <43682392+Gloomylord@users.noreply.github.com>
2020-06-22 15:42:43 +0300

    add loaders

---
+++ loaders/precache-manifest.fea6ce47295009236706021f64f358e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034a9765c2116cd20ce2f82810143f68",
+    "url": "/loaders/index.html"
+  },
+  {
+    "revision": "77fe24510de1252c0a97",
+    "url": "/loaders/static/css/main.957f71cf.chunk.css"
+  },
+  {
+    "revision": "4a02f7447d4c42a67749",
+    "url": "/loaders/static/js/2.a0f7fb59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loaders/static/js/2.a0f7fb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fe24510de1252c0a97",
+    "url": "/loaders/static/js/main.b5908387.chunk.js"
+  },
+  {
+    "revision": "22b2b96fc897fbed2f71",
+    "url": "/loaders/static/js/runtime-main.150a0f30.js"
+  }
+]);
