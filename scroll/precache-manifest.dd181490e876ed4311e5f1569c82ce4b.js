c516db99281254c3533c4fdd501ce7776820d80b
Gloomylord <Saaborteur2012@mail.ru>
2020-07-07 15:21:13 +0300

    add scroll page

---
+++ scroll/precache-manifest.dd181490e876ed4311e5f1569c82ce4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da347ea94aec38811017ee201e2303bb",
+    "url": "/scroll/index.html"
+  },
+  {
+    "revision": "8adfba61a2e5ac4f9b07",
+    "url": "/scroll/static/css/main.63c328b2.chunk.css"
+  },
+  {
+    "revision": "58be1a78c4dda5aa9e91",
+    "url": "/scroll/static/js/2.cd34d553.chunk.js"
+  },
+  {
+    "revision": "f731224685885338d153242cbfb8e85f",
+    "url": "/scroll/static/js/2.cd34d553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adfba61a2e5ac4f9b07",
+    "url": "/scroll/static/js/main.bc248930.chunk.js"
+  },
+  {
+    "revision": "0b1926a8f01e0f918ce8",
+    "url": "/scroll/static/js/runtime-main.1f7be7dc.js"
+  }
+]);
