b2d3bfe7e10a4bfc35524614eb06d83beb6022a9
Gloomylord <Saaborteur2012@mail.ru>
2020-07-08 14:59:50 +0300

    add rubber

---
+++ rubber/precache-manifest.5af93fa4f06e3555c4aa6ce4cdd57040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca99a5abd0050a7908a3377dc471407c",
+    "url": "/rubber/index.html"
+  },
+  {
+    "revision": "57af7de9a3204f9c7b7b",
+    "url": "/rubber/static/css/main.ccfee244.chunk.css"
+  },
+  {
+    "revision": "86387bde904055e29382",
+    "url": "/rubber/static/js/2.de97f8f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rubber/static/js/2.de97f8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57af7de9a3204f9c7b7b",
+    "url": "/rubber/static/js/main.2e26c13f.chunk.js"
+  },
+  {
+    "revision": "af9e2546ed6b071e521e",
+    "url": "/rubber/static/js/runtime-main.f466d0b8.js"
+  }
+]);
