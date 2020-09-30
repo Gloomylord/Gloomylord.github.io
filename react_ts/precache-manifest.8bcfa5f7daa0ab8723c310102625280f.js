3a317a06a9a431dfa8385e142d7fef74e062664d
Dimitry Ivanov <saaboteur2012@yandex.ru>
2020-09-30 21:21:36 +0300

    added react-ts

---
+++ react_ts/precache-manifest.8bcfa5f7daa0ab8723c310102625280f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b300f5f556d1caa5320de10e2b350b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a776a5b7eb126ef22b35",
+    "url": "/static/css/main.5e739815.chunk.css"
+  },
+  {
+    "revision": "e62a2a073a74e57819da",
+    "url": "/static/js/2.ef897dcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ef897dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a776a5b7eb126ef22b35",
+    "url": "/static/js/main.94739789.chunk.js"
+  },
+  {
+    "revision": "542cbb7c0c0bafdf377f",
+    "url": "/static/js/runtime-main.45c10d22.js"
+  }
+]);
